57a5df389ba75aa8c8cf5cff19f6e988943f77fc
bijali-jlakshmi <bijalijayalakshmijayan@gmail.com>
2026-06-07 20:05:20 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Header from "./Header";
+function App() {
+  const [name, setName] = useState("bijali");
+  return (
+    <>
+      <Header variable="bijali" />
+
+      <div
+        style={{
+          backgroundColor: "red",
+          height: "100px",
+          width: "100px",
+          border: "1px solid black",
+          borderRadius: "10px",
+          margin: "20px",
+        }}
+      >
+        text send
+      </div>
+      <div>
+        <h1>State handling - {name}</h1>
+        <button>state chnge btn</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header(props) {
+  return <div>Header {props.variable}</div>;
+}
+
+export default Header;
